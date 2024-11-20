1e4148e4cba5cc4dfac6f9ef15f805e8929d5acc
Rachel Oughton <r.h.oughton@durham.ac.uk>
2024-11-20 10:51:42 +0000

    Updated welcome

--- Rcommands.R
+++ Rcommands.R
@@ -1,28 +1,14 @@
-# work laptop
-setwd("C:/Users/d415ax/OneDrive - Durham University/AP2020_21/Teaching/2023/ClinicalTrials4H/Notes/")
-
-# home laptop
-setwd("C:/Users/Rachel Oughton/OneDrive - Durham University/AP2020_21/Teaching/2023/ClinicalTrials4H/Notes/")
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
 ### for github:
-# home laptop
-setwd("C:/Users/Rachel Oughton/GitRepos/clinicaltrials")
-# work laptop
-setwd("J:/Documents/GitRepos/clinicaltrials")
 # work macbook
-setwd("/Users/rachelo/Documents/GitRepos/clinicaltrials")
+setwd("/Users/rachelo/Documents/GitRepos/ct4")
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 
 
 # for lecture notes
-setwd("/Users/rachelo/Documents/GitRepos/clinicaltrials/lecture_notes")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
-
-#home laptop
-setwd("C:/Users/Rachel Oughton/GitRepos/clinicaltrials/lecture_notes")
+setwd("/Users/rachelo/Documents/GitRepos/ct4/lecture_notes")
 bookdown::render_book("index.Rmd", "bookdown::pdf_book")
 
 
